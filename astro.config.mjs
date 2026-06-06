c8e197076edfe7b764da3601d9dffb752db1e643
Havyn Liew Xiang Rong <liew321@hotmail.com>
2026-06-06 16:37:41 +0800

    several changes

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,14 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import tailwindcss from '@tailwindcss/vite';
+
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://talonexe.github.io',
-    base: '/apu-starter-pack',
+  site: 'https://talonexe.github.io',
+  base: '/apu-starter-pack',
 
+  vite: {
+    plugins: [tailwindcss()],
+  },
 });
