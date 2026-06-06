7a6dcdd11be90c3055e7605f60544b13120f18f1
Havyn Liew Xiang Rong <liew321@hotmail.com>
2026-06-06 16:22:50 +0800

    changed astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,4 +2,8 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+    site: 'https://talonexe.github.io',
+    base: '/apu-starter-pack',
+
+});
